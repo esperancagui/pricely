0ad68e11536982528ba543070b22df801c2e34ed
Guilherme Esperança <guilhermeesp9@gmail.com>
2025-06-07 18:07:50 -0300

    feat(footer) 🎉: footer adicionado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HomeComponent } from './components/home/home.component';
 import { SliderComponent } from './components/slider/slider.component';
 import { CardComponent } from './components/card/card.component';
 import { CategoriesComponent } from './components/categories/categories.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent, SliderComponent, CardComponent, CategoriesComponent],
+  imports: [RouterOutlet, HomeComponent, SliderComponent, CardComponent, CategoriesComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
