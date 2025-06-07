d99ebe3b15f2a2ba5bcffd18e72db5c2afdbd844
Guilherme Esperança <guilhermeesp9@gmail.com>
2025-06-07 17:42:27 -0300

    feat(categories) 🎉: secao de categorias adicionada

--- src/app/components/categories/categories.component.ts
+++ src/app/components/categories/categories.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-categories',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './categories.component.html',
   styleUrl: './categories.component.scss'
 })
 export class CategoriesComponent {
-
+  categories = [
+    { id: 1, name: 'RPG' },
+    { id: 2, name: 'Ação' },
+    { id: 3, name: 'Aventura' },
+    { id: 4, name: 'Estratégia' },
+    { id: 5, name: 'Indie' },
+    { id: 6, name: 'Plataforma' }
+  ];
 }
